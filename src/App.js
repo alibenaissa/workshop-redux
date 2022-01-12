713af3f2806421ec4e6a2ae82ff47f97f729c515
ali <alibenaissa565@gmail.com>
2022-01-12 12:23:23 +0100

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
+
+import {useDispatch,useSelector} from 'react-redux';
 import './App.css';
+import {incrementHandler,decrementHandler,resetHandler} from './Redux/action ';
+import {Button} from 'react-bootstrap'
 
 function App() {
+// create a const for the hook "useDispatch"
+const Dispatch = useDispatch()
+// Dispatch the action 
+const plus =() => { Dispatch (incrementHandler())
+}
+const moin =() => { Dispatch (decrementHandler())
+}
+const zero =() => { Dispatch (resetHandler())
+}
+
+// extract global state from the store 
+const count = useSelector((state)=>state.counter)
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>Work shop react-redux</h1>
+   
+   
+   
+   
+     <div className="Buttons">
+     <Button onClick={plus} variant="success">+</Button>{' '}
+       <h2>{count}</h2>
+     <Button onClick={moin} variant="success">-</Button>{' '}
+     </div>
+     <br/>
+     <br/>
+     <div className="rest">
+     <Button onClick={zero} variant="danger">RESET</Button>{' '}
+     </div>
+
     </div>
   );
 }
